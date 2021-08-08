c8293f06aaa570f34e879c0558eda43700a02df4
JPDBrodeur <jbrodeur001@gmail.com>
2021-08-07 23:51:14 -0400

    prompts, criteria object, charsets

--- script.js
+++ script.js
@@ -1,34 +1,137 @@
-// Assignment code here
+// Creates a global scope for criteria.case
+var criteria = {
+  length: "",
+  case: "",
+  numSpec: ""
+}
+
+var charset = {
+  alpha: "abcdefghijklmnopqrstuvwxyz",
+  numeric: "0123456789",
+  special: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+}
+
 var getCriteria = function() {
-  promptLength();
-  promptCharacters();
+  lengthPrompt();
 }
 
-var promptLength = function() {
-  var length = window.prompt("Please enter the number of characters you wish to include. \nA minimum of 8 characters is required.");
+var lengthPrompt = function() {
+  criteria.length = window.prompt("Please enter the number of characters you wish to include. \n(A minimum of 8 characters is required.)");
+  criteria.length = parseInt(criteria.length);
 
-  if (length < 8) {
+  if (criteria.length < 8) {
     window.alert("Your password must be a minimum of 8 characters.");
-    promptLength();
+    lengthPrompt();
   }
 
-  if (length > 128) {
+  if (criteria.length > 128) {
     window.alert("Your password may be no more than 128 characters.");
-    promptLength();
+    lengthPrompt();
+  }
+
+  if (isNaN(criteria.length)) {
+    window.alert("Please provide a valid response.");
+    lengthPrompt();
+  }
+  // Proceed to next prompt
+  letterPrompt();
+}
+
+var letterPrompt = function() {
+  criteria.case = window.prompt("Please select from the following: \n\n'1' for only LOWERCASE, \n'2' for only UPPERCASE, \n'3' for BOTH upper & lowercase, or \n'4' for NO LETTERS at all.");
+  criteria.case = parseInt(criteria.case);
+  switch (criteria.case) {
+    case 1:
+        var lowercaseOnly = window.confirm("Your password will only contain lowercase letters. \nClick 'Cancel' to select a different option.");
+        if (!lowercaseOnly) {
+          criteria.case = "";
+          letterPrompt();
+        } else {
+        break;
+        }
+    case 2:
+      var uppercaseOnly = window.confirm("Your password will only contain uppercase letters. \nClick 'Cancel' to select a different option.");
+      if (!uppercaseOnly) {
+        criteria.case = "";
+        letterPrompt();
+      } else {
+      break;
+      }
+    case 3:
+      var bothCases = window.confirm("Your password will contain both upper and lowercase letters. \nClick 'Cancel' to select a different option.");
+      if (!bothCases) {
+        criteria.case = "";
+        letterPrompt();
+      } else {
+      break;
+      }
+    case 4:
+      var noLetters = window.confirm("Your password will not contain any letters. \nClick 'Cancel' to select a different option.");
+      if (!noLetters) {
+        criteria.case = "";
+        letterPrompt();
+      } else {
+      break;
+      }
+    default:
+      window.alert("Please provide a valid response.");
+      letterPrompt();
+      break;
+  }
+  // Proceed to next prompt
+  nsPrompt();
+}
+
+var nsPrompt = function() {
+  criteria.numSpec = window.prompt("Which additional character types would you like to include? Select... \n\n'1' for NUMBERS, \n'2' for SPECIAL CHARACTERS, or \n'3' for BOTH numbers and special characters, or \n'4' for NEITHER; only letters");
+  criteria.numSpec = parseInt(criteria.numSpec);
+
+  while (criteria.case === 4 && criteria.numSpec === 4) {
+    window.alert("Your password must contain at least one type of character.");
+    criteria.case = "";
+    criteria.numSpec = '';
+    letterPrompt();
+  }
+
+  switch (criteria.numSpec) {
+    case 1:
+        var numeric = window.confirm("Your password will contain numbers, but no special characters. \nClick 'Cancel' to select a different option.");
+        if (!numeric) {
+          criteria.numSpec = "";
+          nsPrompt();
+        } else {
+        break;
+        }
+    case 2:
+      var specialCharacters = window.confirm("Your password will contain special characters, but no numbers. \nClick 'Cancel' to select a different option.");
+      if (!specialCharacters) {
+        criteria.numSpec = "";
+        nsPrompt();
+      } else {
+      break;
+      }
+    case 3:
+      var bothNumSpec = window.confirm("Your password will contain both numbers and special characters. \nClick 'Cancel' to select a different option.");
+      if (!bothNumSpec) {
+        criteria.numSpec = "";
+        nsPrompt();
+      } else {
+      break;
+      }
+    case 4:
+      var noNumSpec = window.confirm("Your password will not contain any numbers or special characters. \nClick 'Cancel' to select a different option.");
+      if (!noNumSpec) {
+        criteria.numSpec = "";
+        nsPrompt();
+      } else {
+      break;
+      }
+    default:
+      window.alert("Please provide a valid response.");
+      nsPrompt();
+      break;
   }
 }
-
-var promptCharacters = function() {
-  var casePrompt = window.prompt("Please type one of the following to specify case: \n'1' for only lowercase, \n'2' for only uppercase, or \n'3' for both upper & lowercase letters");
-  casePrompt = parseInt(casePrompt);
-    if (casePrompt === 1 || casePrompt === 2 || casePrompt === 3) {
-      // continue to numeric & special characters prompt
-    } else {
-      window.alert("Please provide a valid response.");
-      promptCharacters();
-    }
-}
-
 
 
 // Get references to the #generate element
